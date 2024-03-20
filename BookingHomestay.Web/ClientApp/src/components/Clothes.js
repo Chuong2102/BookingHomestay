6eec0de0dfbedba0346b9a32fd2e6735c4d0763e
Chương Đoàn <chuongdoan2102@gmail.com>
2024-03-20 20:33:18 +0700

    Learn Reactjs: Component, props & state

---
+++ BookingHomestay.Web/ClientApp/src/components/Clothes.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+
+const Clothes = (props) => {
+	console.log(props);
+
+	return (
+		<div>
+			<h1> {props.children} </h1>
+			<ul>
+				<li>
+					<b>Name: </b> {props.name}
+				</li>
+				<li>
+					<b>Type: </b> {props.type}
+				</li>
+				<li>
+					<b>Color:</b> {props.color}
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Clothes;
