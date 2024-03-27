2ee01733160e93bcddedbb202f7085b46e7452c7
Chương Đoàn <chuongdoan2102@gmail.com>
2024-03-27 15:44:16 +0700

    Learn Reactjs - Lifting State Up

--- BookingHomestay.Web/ClientApp/src/components/Home.js
+++ BookingHomestay.Web/ClientApp/src/components/Home.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
 import Clothes from './Clothes';
 
+const USDtoVND = function (props) {
+    const convert = function (usd) {
+        return usd * 23632;
+    };
+
+    return (
+        <div>
+            <span>
+                USD
+            </span>
+            <input
+                onChange={(e) => {
+                    const usd = e.target.value;
+                    const vnd = convert(usd);
+                    props.onHandleChange({
+                        usd,
+                        vnd
+                    });
+
+                }}
+                value={props.value}
+            />
+        </div>
+    );
+};
+
+const VNDtoUSD = function (props) {
+    const convert = function (vnd) {
+        return vnd / 23632;
+    };
+
+    return (
+        <div>
+            <span> VND </span>
+            <input onChange={(e) => {
+                const vnd = e.target.value;
+                const usd = convert(vnd);
+                props.onHandleChange({
+                    usd,
+                    vnd
+                });
+            }}
+
+            value={props.value}
+            />
+        </div>
+    )
+}
+
+
 export class Home extends Component {
     static displayName = Home.name;
 
     constructor(props) {
         super(props);
-        this.state = { index: 1 };
+        this.state = { index: 1, usd: 0, vnd: 0};
 
+    }
+
+    handleChange = (data) => {
+        this.setState(data);
     }
 
   render() {
@@ -21,7 +75,16 @@ export class Home extends Component {
               <br></br>
 
               <div>
+                
+              </div>
+
+              <div>
                   <Clothes name="Jeans" type="Skinny" color="Black" >Quan jeans</Clothes>
+              </div>
+
+              <div style={{ margin: "3%" }}>
+                  <USDtoVND onHandleChange={this.handleChange} value={this.state.usd} />
+                  <VNDtoUSD onHandleChange={this.handleChange} value={this.state.vnd} />
               </div>
         </div>
     );
